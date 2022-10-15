dcb19753a05f4232e5212d8012252f3d0f1a57e0
lathang909 <42415748+lathang909@users.noreply.github.com>
2022-10-16 02:15:08 +0700

    history

--- client/src/components/homepages/history/OrderHistory.js
+++ client/src/components/homepages/history/OrderHistory.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect } from "react";
-import { GlobalState } from "../../../GlobalState";
-import { Link } from "react-router-dom";
-import axios from "axios";
+import React, { useContext, useEffect } from 'react';
+import { GlobalState } from '../../../GlobalState';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function OrderHistory() {
   const state = useContext(GlobalState);
@@ -13,12 +13,12 @@ function OrderHistory() {
     if (token) {
       const getHistory = async () => {
         if (isAdmin) {
-          const res = await axios.get("/api/payment", {
+          const res = await axios.get('/api/payment', {
             headers: { Authorization: token },
           });
           setHistory(res.data);
         } else {
-          const res = await axios.get("/user/history", {
+          const res = await axios.get('/user/history', {
             headers: { Authorization: token },
           });
           setHistory(res.data);
@@ -29,25 +29,39 @@ function OrderHistory() {
   }, [token, isAdmin, setHistory]);
 
   return (
-    <div className="history-page">
-      <h2>History</h2>
+    <div className="history-page" class="overflow-auto">
+      <h2 class="text-center m-20 capitalize md:uppercase tracking-[1.2px]">
+        History
+      </h2>
 
-      <h4>You have {history.length} ordered</h4>
+      <h4 class="text-center m-20 capitalize md:uppercase tracking-[1.2px]">
+        You have {history.length} ordered
+      </h4>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Payment ID</th>
-            <th>Date of Purchased</th>
-            <th></th>
+      <table class="m-auto w-1/2 border border-solid border-black border-collapse">
+        <thead class=" border border-solid  border-black">
+          <tr class="border-solid border border-black border-collapse">
+            <th class="border-solid border border-black text-center border-collapse p-10 capitalize">
+              Payment ID
+            </th>
+            <th class="border-solid border border-black text-center border-collapse p-10 capitalize">
+              Date of Purchased
+            </th>
           </tr>
         </thead>
         <tbody>
           {history.map((items) => (
-            <tr key={items._id}>
-              <td>{items.paymentID}</td>
-              <td>{new Date(items.createdAt).toLocaleDateString()}</td>
-              <td>
+            <tr
+              key={items._id}
+              class="border-solid border border-black text-center border-collapse"
+            >
+              <td class="border-solid border border-black text-center border-collapse p-10 capitalize">
+                {items.paymentID}
+              </td>
+              <td class="border-solid border border-black text-center border-collapse p-10 capitalize">
+                {new Date(items.createdAt).toLocaleDateString()}
+              </td>
+              <td class="border-solid border border-black text-center border-collapse p-10 capitalize">
                 <Link to={`/history/${items._id}`}>View</Link>
               </td>
             </tr>
