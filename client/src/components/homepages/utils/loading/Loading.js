fe80e1f01a11485d58053de1093a9028065a80ac
quinn2403 <66192215+quinn2403@users.noreply.github.com>
2022-10-15 22:25:36 +0700

    loading update

--- client/src/components/homepages/utils/loading/Loading.js
+++ client/src/components/homepages/utils/loading/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div role="status" class="mt-60 mx-auto max-w-sm animate-pulse">
+    <div role="status" class="mt-60 mb-20 mx-auto max-w-sm animate-pulse">
       <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-300 w-48 mb-4"></div>
       <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-300 max-w-[360px] mb-2.5"></div>
       <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-300 mb-2.5"></div>
